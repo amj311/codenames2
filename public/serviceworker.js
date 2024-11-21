d898a67bf47a778040015eda62267d0a04c34ef7
Arthur Judd <amjudd315@gmail.com>
2024-11-21 12:58:35 -0700

    cleanup

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,15 +1,3 @@
-// const urlB64ToUint8Array = (base64String) => {
-// 	const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
-// 	const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
-// 	const rawData = atob(base64);
-// 	const outputArray = new Uint8Array(rawData.length);
-// 	for (let i = 0; i < rawData.length; ++i) {
-// 		outputArray[i] = rawData.charCodeAt(i);
-// 	}
-// 	return outputArray;
-// };
-
-// This code executes in its own worker or thread
 self.addEventListener('install', () => {
 	console.log('Service worker installed');
 });
@@ -80,7 +68,3 @@ self.addEventListener('notificationclick', async (event) => {
 		})()
 	);
 });
-
-// on('push', function (event) {
-// 	console.log('Push event!! ', event.data.text());
-// });
